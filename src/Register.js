9ac57d87feb5da0ad868a6b28330e883e51d2bc1
Andrew Nagy <asnagy@gmail.com>
2024-08-13 16:45:09 -0400

    updated initial draft page to be functional.

--- src/Register.js
+++ src/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { MMKV } from 'react-native-mmkv';
+import uuid from 'react-native-uuid';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
 
 // Main Class
 export default class Register extends Component<Props> {
@@ -21,29 +23,41 @@ export default class Register extends Component<Props> {
 
     // Define State Variables
     this.state = { errorMsg: '',
-                   OKAPI_URL: storage.getString('okapiUrl'),
-                   OKAPI_TENANT: storage.getString('okapiTenant'),
-                   OKAPI_TOKEN: storage.getString('okapiToken'),
-                   CIRC_SERVICEPOINT: storage.getString('servicePointId'),
-                   fieldList: { username: {label: "Username", required: true},
-                                userFirstName: {label: "First Name", required: true},
-                                userLastName: {label: "Last Name", required: true},
-                                userPreferredName: {label: "Preferred Name", required: false},
-                                userBirthDate: {label: "Date of Birth", required: true},
-                                userAddress1: {label: "Address Line 1", required: true},
-                                userAddress2: {label: "Address Line 2", required: false},
-                                userCity: {label: "City", required: true},
-                                userState: {label: "State", required: true},
-                                userZip: {label: "Zip Code", required: true},
-                                userEmail: {label: "Email", required: true},
-                                userPhone: {label: "Phone", required: false}
-                              }
+                    OKAPI_URL: storage.getString('okapiUrl'),
+                    OKAPI_TENANT: storage.getString('okapiTenant'),
+                    OKAPI_TOKEN: storage.getString('okapiToken'),
+                    CIRC_SERVICEPOINT: storage.getString('servicePointId'),
+                    userFirstName: '',
+                    userLastName: '',
+                    userPrefferedName: '',
+                    userBirthDate: '',
+                    userAddress1: '',
+                    userAddress2: '',
+                    userCity: '',
+                    userState: '',
+                    userZip: '',
+                    fieldList: {
+                      username: {label: "Username", required: true},
+                      userFirstName: {label: "First Name", required: true},
+                      userLastName: {label: "Last Name", required: true},
+                      userPreferredName: {label: "Preferred Name", required: false},
+                      userBirthDate: {label: "Date of Birth", required: true},
+                      userAddress1: {label: "Address Line 1", required: true},
+                      userAddress2: {label: "Address Line 2", required: false},
+                      userCity: {label: "City", required: true},
+                      userState: {label: "State", required: true},
+                      userZip: {label: "Zip Code", required: true},
+                      userEmail: {label: "Email", required: true},
+                      userPhone: {label: "Phone", required: false}
+                    }
                 };    
   }
 
-  registerNewUser = (userObj) => {
+  registerNewUser = () => {
+    let userId = uuid.v4();
+    console.log("User Id: " + userId);
 
-    fetch(this.state.OKAPI_URL + "/user/new", {
+    fetch(this.state.OKAPI_URL + "/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -51,22 +65,27 @@ export default class Register extends Component<Props> {
         "x-okapi-token": this.state.OKAPI_TOKEN
       },
       body: JSON.stringify({
-        userFirstName: this.state.userObj['userFirstName'],
-        userPreferredName: this.state.userObj['userPreferredName'],
-        userLastName: this.state.userObj['userLastName'],
-        userEmail: this.state.userObj['userEmail']                
+        id: userId,
+        username: this.state.userEmail,
+        active: false,
+        type: "patron",
+        personal: {
+          firstName: this.state.userFirstName,
+          preferredFirstName: this.state.userPreferredName,
+          lastName: this.state.userLastName,          
+          email: this.state.userEmail
+        }
       })      
     })
     .then((response) => response.json())
     .then((responseJson) => {
-        console.log(responseJson.users[0]["personal"]);
-        this.setState({userObj: responseJson.users[0]["personal"]});
+        console.log(responseJson);
+        this.props.navigation.navigate('RegisterSuccess');
     })
     .catch((error) => {
       console.log("Fetch Error:" + error);
       this.setState({
-          errorMsg: "Error: Unknown User",
-          //errorMsg: error.message,
+          errorMsg: error.message,
         });
     });
 
@@ -76,6 +95,27 @@ export default class Register extends Component<Props> {
   render () {
 
   console.log(this.state.fieldList);
+
+    if (this.state.errorMsg != "") {
+      return (
+        <div className="App">
+          <Container component="main" maxWidth="md">
+            <Alert severity="error">{this.state.errorMsg}</Alert>
+          </Container>
+
+          <Button
+              onClick={() => this.props.navigation.navigate('Home')}
+              fullWidth
+              variant="contained"
+              size="large"              
+              sx={{ mt: 3, mb: 2 }}>
+              Go Back
+          </Button>
+
+        </div>
+      );
+    }
+
 
   return (
     <div className="App">
@@ -94,88 +134,89 @@ export default class Register extends Component<Props> {
 
           <Box component="form" noValidate>
             <TextField
+              onChange={(text) => this.setState({userEmail:text.target.value})}
+              margin="normal"
+              required
+              fullWidth
+              id="userEmail"
+              label="Email"
+              name="userEmail"
+              autoFocus
+            />
+            <TextField
+              onChange={(text) => this.setState({userFirstName:text.target.value})}
               margin="normal"
               required
               fullWidth
               id="userFirstName"
               label="First Name"
               name="userFirstName"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userLastName:text.target.value})}
               margin="normal"
               required
               fullWidth
               id="userLastName"
               label="Last Name"
               name="userLastName"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userPreferredName:text.target.value})}
               margin="normal"
               fullWidth
               id="userPreferredName"
               label="Preferred Name"
               name="userPreferredName"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userBirthDate:text.target.value})}            
               margin="normal"
               required
               fullWidth
               id="userBirthDate"
               label="Birth Date"
               name="userBirthDate"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userAddress1:text.target.value})}            
               margin="normal"
               fullWidth
               id="userAddressLine1"
               label="Address"
               name="userAddressLine1"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userAddress2:text.target.value})}            
               margin="normal"
               fullWidth
               id="userAddressLine2"
               label="Address"
               name="userAddressLine2"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userCity:text.target.value})}            
               margin="normal"
               fullWidth
               id="userAddressCity"
               label="City"
               name="userAddressCity"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userState:text.target.value})}            
               margin="normal"
               fullWidth
               id="userAddressState"
               label="State"
               name="userAddressState"
-              autoComplete=""
-              autoFocus
             />
             <TextField
+              onChange={(text) => this.setState({userZip:text.target.value})}                        
               margin="normal"
               fullWidth
               id="userAddressZip"
               label="Zip Code"
               name="userAddressZip"
-              autoComplete=""
-              autoFocus
             />
 
             <Button
